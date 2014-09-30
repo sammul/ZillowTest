abb9fdde5c0801266a37ed67548b54fafa7a5fa1
Sammul Wu <wuxia1989@gmail.com>
2014-09-30 11:25:14 -0700

    9/30

--- MyString.java
+++ MyString.java
@@ -1,7 +1,7 @@
 /**
  * In this class MyString, I implemented a method "stringToLong". It supports an optional sign at the first character.
  * Legal input: 10, +10, -55...
- * Limitations: (1) If the input is illegal (ex. 100s0), the method will throw an exception containing message "Illegal Input String".
+ * Limitations: (1) If the input is illegal (ex. null, empty string, 100s0), the method will throw an exception containing message "Illegal Input String".
  * (2) If the input is larger than Long.MAX_VALUE, the method will throw an exception containing message "MAX OVERFLOW".
  * (3) If the input is smaller than Long.MIN_VALUE, the method will throw an exception containing message "MIN_OVERFLOW".
  * 
@@ -18,12 +18,16 @@ public class MyString{
 		long res = 0, lmax=Long.MAX_VALUE%10, lmin=Math.abs(Long.MIN_VALUE%10), overflow = Long.MAX_VALUE/10;
 		long prefix = 1;
 		int i = 0;
+		if(s==null||s.length()==0)
+			throw new IllegalArgumentException("Illegal Input String");
 		if(s.charAt(0)=='-'){
 			prefix = -1;
 			i=1;
 		}
 		else if(s.charAt(0)=='+')
 			i=1;
+		if(i==s.length())
+			throw new IllegalArgumentException("Illegal Input String");
 		for(; i<s.length(); i++){
 		    int c = s.charAt(i)-'0';
 		    if(c>9 || c<0)
